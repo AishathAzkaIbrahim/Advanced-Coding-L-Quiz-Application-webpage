f5ae3e70ef3930b577f2c90928c2d45e35b25ecf
Mohamed Atho Atho <>
2022-08-29 08:55:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+Red = "\033[0;31m"
+Green ="\033[0;32m"
+Yellow = "\033[0;33m"
+Blue = "\033[0;34m"
+
+answer1 = input(Red + 'Hello! How are you?' + Green)
+
+answer2 = input(Blue + 'What is your name?' + Yellow)
+
+answer3 = input(Yellow + 'Did you know that this quiz is about Our World and Outer Space?' + Red)
+
+answer4 = input(Blue + 'How old is planet Earth?' + Green)
+if answer4 == '4 billion years':
+  print('That is correct!' + Green)
+else:
+  print('Sorry! That is the wrong answer!')
+
+  answer5 = input(Blue + 'How many planets are there in the Solar System?' + Green)
+if answer5 == 'Eight' or '8':
+  print('That is correct' + Green)
+else:
+  print('Sorry! That is the wrong answer!')
+
+answer6 = input(Blue + 'What is the name of the biggest planet?' + Green)
+if answer6 == 'Jupiter':
+  print('That is correct' + Green)
+else:
+  print('Sorry! That is the wrong answer!')
+
+answer7 = input(Blue + 'What planet is famous for its lovely rings?' + Green)
+if answer7 == 'Saturn':
+  print('That is correct' + Green)
+else:
+  print('Sorry! That is the wrong answer!')
+
+answer8 = input(Blue + 'Who was the first person who walked on the Moon?' + Green)
+if answer == 'Neil Armstrong':
+else:
+    print('Sorry! That is the wrong answer!')
+    
+answer9 = input(Blue + 'Is teh sun a planet or a star?' + Green)
+if answer9 == 'a star':
+  print('That is correct!' + Green)
+
+answer10 = input(Blue + 'What is the hottest planet in the Solar System?' + Green)
+if answer10 == 'Venus':
+  print('That is correct!' + Green)
+
+answer11 = input(Blue + 'How many continents are there on Earth?' + Green)
+if answer11 == '7' or 'Seven':
+  print('That is correct!' + Green)
+
+answer12 = input(Blue + 'What is the name of the biggest ocean on Earth?' + Green)
+if answer12 == 'Pacific Ocean':
+  print('That is correct!' + Green)
+
